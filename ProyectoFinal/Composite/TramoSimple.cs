f2331c5f3f44dd8d06cce0afa1248b0799a7915f
ODaniel22 <166674672+ODaniel22@users.noreply.github.com>
2025-05-10 17:47:32 -0400

    Add files via upload

---
+++ ProyectoFinal/Composite/TramoSimple.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal.Composite
+{
+    public class TramoSimple : Itramo
+    {
+        private string _destino;
+        private double _precio;
+
+        public TramoSimple(string destino, double precio)
+        {
+            _destino = destino;
+            _precio = precio;
+        }
+
+        public double CalcularPrecio()
+        {
+            return _precio;
+        }
+
+        public string ObtenerDescripcion()
+        {
+            return $"Destino: {_destino}, Precio: {_precio}$";
+        }
+
+
+    }
+}
+
